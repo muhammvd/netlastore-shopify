9c02902bd53b9d7f2fa1e3c42ff8ad4470887535
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2023-01-02 20:03:19 +0100

    workflow setup

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const themeKit = require('@shopify/themekit');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function() {
+    return gulp.src('scss/global.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets'))
+
+})
+
+gulp.task ('watch', function(){
+    gulp.watch('scsss/*.scss', gulp.series('sass'))
+    /*.themeKit.command('watch', {
+        allowLive: true,
+        env: 'development'
+    }) */
+});
