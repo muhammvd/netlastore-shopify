1528132befa0d46df8341e102a7a2b3233998209
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2023-01-03 08:57:08 +0100

    created a minify function with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 const gulp = require('gulp');
 const themeKit = require('@shopify/themekit');
 const sass = require('gulp-sass')(require('sass'));
+const clean = require('gulp-clean-css');
 
 gulp.task('sass', function() {
     return gulp.src('scss/global.scss')
     .pipe(sass())
+    .pipe(clean({compatibility: 'ie11'}))
     .pipe(gulp.dest('assets'))
 
 })
+
+//minify css code
+
 
 //Feature deprecated
 
